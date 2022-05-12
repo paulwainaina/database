b882e791bf7232cf0204a940395802930758ae38
paulwainaina <paulwainaina36@gmail.com>
2022-05-12 15:06:20 +0300

    update,delete,distinct

--- Database.go
+++ Database.go
@@ -3,6 +3,7 @@ package database
 import (
 	"context"
 	"time"
+
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -28,7 +29,7 @@ func (db *Database) Connect() bool {
 	if db.Err != nil {
 		return false
 	}
-	if 	db.Err = db.Client.Connect(db.Ctx);db.Err != nil {
+	if db.Err = db.Client.Connect(db.Ctx); db.Err != nil {
 		return false
 	}
 	return true
@@ -50,7 +51,7 @@ func (db *Database) Ping() bool {
 	return true
 }
 
-func (db *Database) QueryOne(database string, col string, filter interface{}) (bson.M) {
+func (db *Database) QueryOne(database string, col string, filter interface{}) bson.M {
 	_col := db.Client.Database(database).Collection(col)
 	db.Ctx, db.Cancel = context.WithTimeout(context.Background(), 10*time.Second)
 	var result bson.M
@@ -58,7 +59,7 @@ func (db *Database) QueryOne(database string, col string, filter interface{}) (b
 	return result
 }
 
-func (db *Database) InsertOne(database string, col string, doc interface{}) (interface{}) {
+func (db *Database) InsertOne(database string, col string, doc interface{}) interface{} {
 	_col := db.Client.Database(database).Collection(col)
 	var result interface{}
 	db.Ctx, db.Cancel = context.WithTimeout(context.Background(), 10*time.Second)
@@ -66,7 +67,7 @@ func (db *Database) InsertOne(database string, col string, doc interface{}) (int
 	return result
 }
 
-func (db* Database) InsertMany(database string, col string, doc []interface{})(interface{}){
+func (db *Database) InsertMany(database string, col string, doc []interface{}) interface{} {
 	_col := db.Client.Database(database).Collection(col)
 	db.Ctx, db.Cancel = context.WithTimeout(context.Background(), 10*time.Second)
 	var result interface{}
@@ -74,14 +75,63 @@ func (db* Database) InsertMany(database string, col string, doc []interface{})(i
 	return result
 }
 
-func (db *Database) QueryMany(database string, col string,filter interface{}) ([]bson.M) {
+func (db *Database) QueryMany(database string, col string, filter interface{}) []bson.M {
 	_col := db.Client.Database(database).Collection(col)
-	db.Ctx, db.Cancel = context.WithTimeout(context.Background(), 10*time.Second)	
-	cusor, err := _col.Find(db.Ctx,filter)
-	result:=[] bson.M{}
-	if err==nil{
-		err=cusor.All(db.Ctx,&result)
+	db.Ctx, db.Cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	cusor, err := _col.Find(db.Ctx, filter)
+	result := []bson.M{}
+	if err == nil {
+		db.Err = cusor.All(db.Ctx, &result)
 	}
-	db.Err=err
+	return result
+}
+
+func (db *Database) UpdateOne(database string, col string, filter interface{}, update interface{}, options *options.UpdateOptions) interface{} {
+	_col := db.Client.Database(database).Collection(col)
+	db.Ctx, db.Cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	result, err := _col.UpdateOne(db.Ctx, filter, update, options)
+	if err != nil {
+		db.Err = err
+	}
+	return result
+}
+
+func (db *Database) UpdateMany(database string, col string, filter interface{}, update interface{}, options *options.UpdateOptions) interface{} {
+	_col := db.Client.Database(database).Collection(col)
+	db.Ctx, db.Cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	result, err := _col.UpdateMany(db.Ctx, filter, update, options)
+	if err != nil {
+		db.Err = err
+	}
+	return result
+}
+
+func (db *Database) DeleteOne(database string, col string, filter interface{}, delete *options.DeleteOptions) interface{} {
+	_col := db.Client.Database(database).Collection(col)
+	db.Ctx, db.Cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	result, err := _col.DeleteOne(db.Ctx, filter, delete)
+	if err != nil {
+		db.Err = err
+	}
+	return result
+}
+
+func (db *Database) DeleteMany(database string, col string, filter interface{}, delete *options.DeleteOptions) interface{} {
+	_col := db.Client.Database(database).Collection(col)
+	db.Ctx, db.Cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	result, err := _col.DeleteMany(db.Ctx, filter, delete)
+	if err != nil {
+		db.Err = err
+	}
+	return result
+}
+
+func (db *Database) FindDistinct(database string, col string, filter string, distinct *options.DistinctOptions) interface{} {
+	_col := db.Client.Database(database).Collection(col)
+	db.Ctx, db.Cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	result, err := _col.Distinct(db.Ctx, filter, distinct)
+	if err != nil {
+		db.Err = err
+	}
 	return result
 }
