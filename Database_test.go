86c49e798dc819a5c7af14aa0ef0aefa0a5d8842
paulwainaina <paulwainaina36@gmail.com>
2022-05-16 11:37:50 +0300

    FindDistinct, UpdateOne, UpdateMany, DeleteOne, DeleteMany

--- Database_test.go
+++ Database_test.go
@@ -3,9 +3,11 @@ package database
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var database = Database{DbName: "example", DbServer: "mongodb://localhost:27017"}
@@ -93,4 +95,67 @@ func TestInsertManyQueryMany(t *testing.T) {
 	if database.Err != nil {
 		t.Errorf("Error closing connection %q", database.Err)
 	}
+}
+
+func TestUpdateOne(t *testing.T){
+	database.Connect()
+	person := Person{Name: "test", Address: Address{City: "NewYork"}}
+	database.InsertOne(database.DbName, "users", person)
+	if database.Err != nil {
+		t.Errorf("Error closing connection %q", database.Err)
+	}
+	filter:=bson.M{"name":"test"}
+	update:=bson.M{"$set":bson.M{"name":"test1"}}
+	var options *options.UpdateOptions
+	database.UpdateOne(database.DbName,"users",filter,update,options)
+	if database.Err != nil {
+		t.Errorf("Error closing connection %q", database.Err)
+	}
+}
+
+func TestUpdateMany(t *testing.T){
+	database.Connect()
+	person := Person{Name: "test", Address: Address{City: "NewYork"}}
+	database.InsertOne(database.DbName, "users", person)
+	if database.Err != nil {
+		t.Errorf("Error closing connection %q", database.Err)
+	}
+	filter:=bson.M{}
+	update:=bson.M{"$set":bson.M{"name":"test2"}}
+	var options *options.UpdateOptions
+	database.UpdateMany(database.DbName,"users",filter,update,options)
+	if database.Err != nil {
+		t.Errorf("Error closing connection %q", database.Err)
+	}
+}
+
+func TestDeleteOne(t *testing.T){
+	database.Connect()
+	filter := bson.M{"$or":[]bson.M{{"name":"test"},{"name":"test1"}}}
+	var options *options.DeleteOptions
+	database.DeleteOne(database.DbName,"users",filter,options)
+	if database.Err != nil {
+		t.Errorf("Error closing connection %q", database.Err)
+	}
+}
+
+func TestDeleteMany(t *testing.T){
+	database.Connect()
+	filter := bson.M{"$or":[]bson.M{{"name":"test"},{"name":"test1"}}}
+	var options *options.DeleteOptions
+	database.DeleteMany(database.DbName,"users",filter,options)
+	if database.Err != nil {
+		t.Errorf("Error closing connection %q", database.Err)
+	}
+}
+
+func TestFindDistinct(t *testing.T){
+	database.Connect()
+	var filter string="name"
+	options:=  options.Distinct().SetMaxTime(2 * time.Second)
+	result:=database.FindDistinct(database.DbName,"users",filter,options)
+	if database.Err != nil {
+		t.Errorf("Error closing connection %q", database.Err)
+	}
+	fmt.Println(result)
 }
--- README.md
+++ README.md
@@ -18,7 +18,7 @@ or
 `filter := bson.M{"$or":[]bson.M{{"name":"test"},{"name":"test1"}}}`
 `database.QueryMany(database.DbName, "users", filter)`
 
-The **QueryMany** and **QueryOne** requires that you pass in the database name **DbName** , the collection and a filter which is a **bson object**
+The **QueryMany** and **QueryOne** requires that you pass in the database name **DbName** , the collection and a filter which is a **bson object**.
 
 3. Inserting records.
 
@@ -33,4 +33,42 @@ or
 `person := Person{Name: "test", Address: Address{City: "NewYork"}}`
 `database.InsertOne(database.DbName, "users", person)`
 
-The **InsertMany** and **InsertOne** requires that you pass in the database name **DbName** , the collection and a filter which is a **bson object**
+The **InsertMany** and **InsertOne** requires that you pass in the database name **DbName** , the collection and a filter which is a **bson object**.
+
+4. Delete records.
+
+`filter := bson.M{"$or":[]bson.M{{"name":"test"},{"name":"test1"}}}`
+`var options *options.DeleteOptions`
+`database.DeleteOne(database.DbName,"users",filter,options)`
+
+or
+
+`filter := bson.M{"$or":[]bson.M{{"name":"test"},{"name":"test1"}}}`
+`var options *options.DeleteOptions`
+`database.DeleteMany(database.DbName,"users",filter,options)`
+
+The **DeleteOne** and **Deletemany** requires that you pass in the database name **DbName** , the collection and a filter which is a **bson object** and additional options.
+
+5. Update records.
+
+`filter:=bson.M{}`
+`update:=bson.M{"$set":bson.M{"name":"test2"}}`
+`var options *options.UpdateOptions`
+`database.UpdateMany(database.DbName,"users",filter,update,options)`
+
+or
+
+`filter:=bson.M{"name":"test"}`
+`update:=bson.M{"$set":bson.M{"name":"test1"}}`
+`var options *options.UpdateOptions`
+`database.UpdateOne(database.DbName,"users",filter,update,options)`
+
+The **UpdateOne** and **UpdateMany** requires that you pass in the database name **DbName** , the collection and a filter which is a **bson object** and additional options.
+
+6. Distinct records.
+
+`var filter string="name"`
+`options:=  options.Distinct().SetMaxTime(2 * time.Second)`
+`result:=database.FindDistinct(database.DbName,"users",filter,options)`
+
+The **FindDistinct** requires that you pass in the database name **DbName** , the collection and a filter which is a **bson object** and additional options.	
